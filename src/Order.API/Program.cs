f45ff34fb92d7da8a41e83599be600f9cd085ea4
anapeniche <anaclaudiapeniche120@gmail.com>
2026-06-14 18:31:56 -0300

    feat: add Serilog structured logging with JSON formatter to both services

--- src/Order.API/Program.cs
+++ src/Order.API/Program.cs
@@ -1,37 +1,59 @@
 using Microsoft.EntityFrameworkCore;
 using Order.API.Data;
 using Order.API.Services;
+using Serilog;
+using Serilog.Formatting.Compact;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(new CompactJsonFormatter())
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Service", "Order.API")
+    .CreateLogger();
 
-builder.Services.AddDbContext<OrderDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddHostedService<Order.API.StockResponseWorker>();
-builder.Services.AddHostedService<OutboxPublisher>();
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
+try
 {
-    var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
-    try
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddDbContext<OrderDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddHostedService<Order.API.StockResponseWorker>();
+    builder.Services.AddHostedService<OutboxPublisher>();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
     {
-        db.Database.Migrate();
-        Console.WriteLine("[DB] Migrations aplicadas com sucesso.");
+        var db = scope.ServiceProvider.GetRequiredService<OrderDbContext>();
+        try
+        {
+            db.Database.Migrate();
+            Log.Information("Migrations aplicadas com sucesso");
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Erro ao aplicar migrations");
+        }
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"[ERRO DB]: {ex.Message}");
-    }
+
+    app.UseSerilogRequestLogging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Order.API encerrou inesperadamente");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.MapControllers();
-app.Run();
 public partial class Program { }
--- src/Stock.API/Program.cs
+++ src/Stock.API/Program.cs
@@ -1,28 +1,49 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Formatting.Compact;
 using Stock.API.Data;
 using Stock.API;
 
-var builder = Host.CreateApplicationBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(new CompactJsonFormatter())
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Service", "Stock.API")
+    .CreateLogger();
 
-builder.Services.AddDbContext<StockDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-
-using (var scope = host.Services.CreateScope())
+try
 {
-    var db = scope.ServiceProvider.GetRequiredService<StockDbContext>();
-    try
-    {
-        db.Database.Migrate();
-        Console.WriteLine("[DB] Stock migrations aplicadas com sucesso.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"[ERRO DB STOCK]: {ex.Message}");
-    }
-}
+    var builder = Host.CreateApplicationBuilder(args);
 
-host.Run();
+    builder.Services.AddSerilog();
+
+    builder.Services.AddDbContext<StockDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    builder.Services.AddHostedService<Worker>();
+
+    var host = builder.Build();
+
+    using (var scope = host.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<StockDbContext>();
+        try
+        {
+            db.Database.Migrate();
+            Log.Information("Stock migrations aplicadas com sucesso");
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Erro ao aplicar Stock migrations");
+        }
+    }
+
+    host.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Stock.API encerrou inesperadamente");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
